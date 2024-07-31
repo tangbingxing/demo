458c9c6a2449681b8affc06281db7a89b7804429
tangbingxing <tangbingxingtpx@gmail.com>
2024-07-31 19:26:08 +0800

    hotel-demo:新增文档

--- hotel-demo/src/test/java/com/tangbingxing/hotel/HotelDocumentTest.java
+++ hotel-demo/src/test/java/com/tangbingxing/hotel/HotelDocumentTest.java
@@ -1,9 +1,15 @@
 package com.tangbingxing.hotel;
 
+import com.alibaba.fastjson.JSON;
+import com.tangbingxing.hotel.pojo.Hotel;
+import com.tangbingxing.hotel.pojo.HotelDoc;
 import com.tangbingxing.hotel.service.IHotelService;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,8 +27,22 @@ class HotelDocumentTest {
     @Autowired
     private IHotelService hotelService;
 
+    //新增文档
     @Test
     void testAddDocument() throws IOException {
+        // 1.根据id查询酒店数据
+        Hotel hotel = hotelService.getById(61083L);
+        // 2.转换为文档类型
+        HotelDoc hotelDoc = new HotelDoc(hotel);
+        // 3.将HotelDoc转json
+        String json = JSON.toJSONString(hotelDoc);
+
+        // 1.准备Request对象
+        IndexRequest request = new IndexRequest("hotel").id(hotelDoc.getId().toString());
+        // 2.准备Json文档
+        request.source(json, XContentType.JSON);
+        // 3.发送请求
+        client.index(request, RequestOptions.DEFAULT);
     }
 
     @Test
